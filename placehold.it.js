bc57478cb3118db6ae2006c3e38c757541acd437
David Vogeleer <david@individual11.com>
2011-03-01 15:08:06 +0000

    Added support for http://placekitten.com/ and tweeked the conditionals a tiny bit.. see documented code for more details

--- placehold.it.js
+++ placehold.it.js
@@ -1,13 +1,28 @@
 /*//////////////////////
 
 Author: David Vogeleer
-Site: http://www.individual11.com
+Site: http://www.individual11.com/placeholdit/
+Description: The easiest way to add FPO images to your mockups at runtime.
 
-Version: 0.2
+Version: 0.3
 
 License: MIT, see README.txt
 
-**Placehold.it is a service/website created by Brent Spore (http://twitter.com/#!/iboughtamac) and Russell Heimlich (http://twitter.com/#!/kingkool68)**
+** Placehold.it is a service/website created by Brent Spore (http://twitter.com/#!/iboughtamac) and Russell Heimlich (http://twitter.com/#!/kingkool68) **
+** Placekitten.com is a service created by Mark James (http://mark.james.name/) **
+
+*****CHANGE LOG*****
+
+-V 0.3
+•Added support for 'http://placekitten.com/' because KITTENS ARE AWESOME! ( see settings for details )
+	•if the background color is '000' or '000000' then the kitten image returned will be black & white
+	•format, text, textcolor are ignored if type is set to 'kitten', the others will still work
+•Removed the conditional for adding text for Placehold.it calls, since the server ignores blank text anyway, so hopefully it will speed the loop up a little bit
+•Changed Site reference in this file so it points directly to where the example file lives
+•Added Description, just in case
+
+-V 0.2
+• Looks at img attributes (or css) to see if it should already have a specific width x height
 
 *//////////////////////
 
@@ -22,7 +37,8 @@ License: MIT, see README.txt
     	'width'				:	200,		//->required
     	'height'			:	'',			//->not required. If left blank, image returned will be square widthxwidth
     	'overwriteSRC'		:	false,		//->if the image already has src filled in, this decided whether or not to replace it
-    	'overrideProperties':	false		//->by default, if the img element has width/height set, we use that, but if this is set to true, we ignore it and use the settings
+    	'overrideProperties':	false,		//->by default, if the img element has width/height set, we use that, but if this is set to true, we ignore it and use the settings
+    	'type'				:	'default'	//->'default' uses Placehold.it to generate images, 'kitten' uses placekitten.com (if you pass '000' or '000000' as the background color, the images will be b&w)
     }
     
     //make sure at least one image is there
@@ -33,17 +49,24 @@ License: MIT, see README.txt
     	
     	return this.each(function(i, el){
     		if(settings.overwriteSRC || !$(el).attr('src')){
-	    		var src = 'http://placehold.it/';
     			var width = (settings.overrideProperties)? Number(settings.width):Number($(el).attr('width')) ||  settings.width;
     			var height = (settings.overrideProperties)? Number(settings.height) || width:Number($(el).attr('height')) || Number(settings.height) || width;
-    			src += width + "x" + height;
-    			src += "/" + settings.backgroundColor + "/" + settings.textColor;
-    			src += settings.format;
-    			if(settings.text.length) src += "&text=" + settings.text;
+    			if(settings.type === 'kitten'){
+    				var src = 'http://placekitten.com/';
+    				if(settings.backgroundColor === '000' || settings.backgroundColor === '000000') src += 'g/';
+    				src += width + '/' + height;
+    			}else{
+    				var src = 'http://placehold.it/';
+    				src += width + "x" + height;
+    				src += "/" + settings.backgroundColor + "/" + settings.textColor;
+    				src += settings.format;
+    				src += "?text=" + settings.text;//the service ignores it if it's left blank, so this will speed up the loop slightly
+    			}
     			$(el).attr('src', src);
     		}
     	});
     }else{
+    	//if no images are found, it just moves forward
     	return this;
     }
   };
